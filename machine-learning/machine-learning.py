7e907f1a11872e580cf50f3744c5a89fb3f3b503
Matthew BL <xamtehwt@gmail.com>
2024-06-04 09:54:57 +0200

    Cache and not cache models distinguished; Request time outliers managed

--- machine-learning/machine-learning.py
+++ machine-learning/machine-learning.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import joblib
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_regression
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import RobustScaler
 from math import sqrt
 
 # Read the backend_access_data.csv file
@@ -24,10 +21,26 @@ merged_data = pd.merge(backend_data, frontend_data, on='Request ID', suffixes=('
 
 merged_data = pd.merge(merged_data, metrics_data, on='Endpoint', suffixes=('_request', '_metric'))
 
-def feature_selection(target_attribute, merged_data):
+# Group the rows by endpoints and calculate the average request time of each endpoint
+grouped_data = merged_data.groupby('Endpoint')['Request Time'].mean()
+
+# Add a new feature "average request time", and give it the value obtained earlier
+merged_data['Average Request Time'] = merged_data['Endpoint'].map(grouped_data)
+
+# Initialize a Robust Scaler
+scaler = RobustScaler()
+
+# Fit the scaler to the 'Request Time' column and transform it
+merged_data['Request Time'] = scaler.fit_transform(merged_data[['Request Time']])
+
+# Split data into two datasets: one with isCached as true and another as false
+cached_data = merged_data[merged_data['isCached'] == True]
+not_cached_data = merged_data[merged_data['isCached'] == False]
+
+def feature_selection(target_attribute, data):
     # Split the data into input and output
-    X = merged_data.drop(['CPU Usage', 'Memory Usage', 'Storage Usage', 'Request ID', 'Query', 'Endpoint'], axis=1)
-    Y = merged_data[target_attribute]
+    X = data.drop(['CPU Usage', 'Memory Usage', 'Storage Usage', 'Request ID', 'Query', 'Endpoint'], axis=1)
+    Y = data[target_attribute]
 
     categorical_columns = ['HTTP method', 'Pricing Plan', 'Response Status', 'Cache Used']
     X = pd.get_dummies(X, columns=categorical_columns)
@@ -47,18 +60,24 @@ def feature_selection(target_attribute, merged_data):
 
     return X.columns[sfm.get_support(indices=True)]
 
-cpu_usage_feature_selection = feature_selection('CPU Usage', merged_data)
-memory_usage_feature_selection = feature_selection('Memory Usage', merged_data)
-storage_usage_feature_selection = feature_selection('Storage Usage', merged_data)
+cpu_usage_feature_selection_cache = feature_selection('CPU Usage', cached_data)
+cpu_usage_feature_selection_not_cache = feature_selection('CPU Usage', not_cached_data)
+memory_usage_feature_selection_cache = feature_selection('Memory Usage', cached_data)
+memory_usage_feature_selection_not_cache = feature_selection('Memory Usage', not_cached_data)
+storage_usage_feature_selection_cache = feature_selection('Storage Usage', cached_data)
+storage_usage_feature_selection_not_cache = feature_selection('Storage Usage', not_cached_data)
 
-print('CPU Usage: ' + str(cpu_usage_feature_selection))
-print('Memory Usage: '+ str(memory_usage_feature_selection))
-print('Storage Usage: '+ str(storage_usage_feature_selection))
+print('Cached CPU Usage: ' + str(cpu_usage_feature_selection_cache))
+print('Not cached CPU Usage: ' + str(cpu_usage_feature_selection_not_cache))
+print('Cached Memory Usage: '+ str(memory_usage_feature_selection_cache))
+print('Not cached Memory Usage: '+ str(memory_usage_feature_selection_not_cache))
+print('Cached Storage Usage: '+ str(storage_usage_feature_selection_cache))
+print('Not cached Storage Usage: '+ str(storage_usage_feature_selection_not_cache))
 
-def train_model(target_attribute, selected_feature_names, merged_data, model=GradientBoostingRegressor()):
+def train_model(target_attribute, selected_feature_names, data, model=GradientBoostingRegressor()):
     # Select the features from the data
-    X = merged_data
-    Y = merged_data[target_attribute]
+    X = data
+    Y = data[target_attribute]
 
     categorical_columns = ['HTTP method', 'Pricing Plan', 'Response Status', 'Cache Used']
     X = pd.get_dummies(X, columns=categorical_columns)
@@ -95,20 +114,33 @@ def predict(model, new_data):
 
 model = GradientBoostingRegressor()
 
-cpu_usage_model, cpu_usage_evaluation = train_model('CPU Usage', cpu_usage_feature_selection, merged_data, model)
-memory_usage_model, memory_usage_evaluation = train_model('Memory Usage', memory_usage_feature_selection, merged_data, model)
-storage_usage_model, storage_usage_evaluation = train_model('Storage Usage', storage_usage_feature_selection, merged_data, model)
+cpu_usage_model_cache, cpu_usage_evaluation_cache = train_model('CPU Usage', cpu_usage_feature_selection_cache, data, model)
+memory_usage_model_cache, memory_usage_evaluation_cache = train_model('Memory Usage', memory_usage_feature_selection_cache, data, model)
+storage_usage_model_cache, storage_usage_evaluation_cache = train_model('Storage Usage', storage_usage_feature_selection_cache, data, model)
 
-print('CPU Usage: ' + str(cpu_usage_evaluation))
-print('Memory Usage: '+ str(memory_usage_evaluation))
-print('Storage Usage: '+ str(storage_usage_evaluation))
+cpu_usage_model_not_cache, cpu_usage_evaluation_not_cache = train_model('CPU Usage', cpu_usage_feature_selection_not_cache, data, model)
+memory_usage_model_not_cache, memory_usage_evaluation_not_cache = train_model('Memory Usage', memory_usage_feature_selection_not_cache, data, model)
+storage_usage_model_not_cache, storage_usage_evaluation_not_cache = train_model('Storage Usage', storage_usage_feature_selection_not_cache, data, model)
+
+print('Cached CPU Usage: ' + str(cpu_usage_evaluation_cache))
+print('Not cached CPU Usage: ' + str(cpu_usage_evaluation_not_cache))
+print('Cached Memory Usage: '+ str(memory_usage_evaluation_cache))
+print('Not cached Memory Usage: '+ str(memory_usage_evaluation_not_cache))
+print('Cached Storage Usage: '+ str(storage_usage_evaluation_cache))
+print('Not cached Storage Usage: '+ str(storage_usage_evaluation_not_cache))
 
 # Save the models to disk
-joblib.dump(cpu_usage_model, 'cpu_usage_model.pkl')
-joblib.dump(memory_usage_model, 'memory_usage_model.pkl')
-joblib.dump(storage_usage_model, 'storage_usage_model.pkl')
+joblib.dump(cpu_usage_model_cache, 'cpu_usage_model_cache.pkl')
+joblib.dump(cpu_usage_model_not_cache, 'cpu_usage_model_not_cache.pkl')
+joblib.dump(memory_usage_model_cache, 'memory_usage_model_cache.pkl')
+joblib.dump(memory_usage_model_not_cache, 'memory_usage_model_not_cache.pkl')
+joblib.dump(storage_usage_model_cache, 'storage_usage_model_cache.pkl')
+joblib.dump(storage_usage_model_not_cache, 'storage_usage_model_not_cache.pkl')
 
 # Save the selected features to disk
-joblib.dump(cpu_usage_feature_selection, 'cpu_usage_features.pkl')
-joblib.dump(memory_usage_feature_selection, 'memory_usage_features.pkl')
-joblib.dump(storage_usage_feature_selection, 'storage_usage_features.pkl')
+joblib.dump(cpu_usage_feature_selection_cache, 'cpu_usage_features_cache.pkl')
+joblib.dump(cpu_usage_feature_selection_not_cache, 'cpu_usage_features_not_cache.pkl')
+joblib.dump(memory_usage_feature_selection_cache, 'memory_usage_features_cache.pkl')
+joblib.dump(memory_usage_feature_selection_not_cache, 'memory_usage_features_not_cache.pkl')
+joblib.dump(storage_usage_feature_selection_cache, 'storage_usage_features_cache.pkl')
+joblib.dump(storage_usage_feature_selection_not_cache, 'storage_usage_features_not_cache.pkl')
